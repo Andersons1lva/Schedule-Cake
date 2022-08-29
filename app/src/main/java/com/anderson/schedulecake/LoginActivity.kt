8a5441a3c1a16c9b6b16e4d62c6fd9ac82b28b44
andersons1lva <andersons1lva@yahoo.com.br>
2022-08-29 15:57:11 -0300

    alter activity_login.xml.

--- app/src/main/java/com/anderson/schedulecake/LoginActivity.kt
+++ app/src/main/java/com/anderson/schedulecake/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.anderson.schedulecake
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.anderson.schedulecake.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        //chamada da activity Registration
+        val account = findViewById<TextView>(R.id.tv_not_account)
+        account.setOnClickListener{
+            notaccount()
+        }
+
+    }
+
+    private fun notaccount() {
+        val intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
+        startActivity(intent)
     }
 }
