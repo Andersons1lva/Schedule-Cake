719fd030929100aa734734fc9a6989f004c2adc0
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-22 15:25:50 -0300

    Bottom menu creation.

--- app/src/main/java/com/anderson/schedulecake/MainActivity.kt
+++ app/src/main/java/com/anderson/schedulecake/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.anderson.schedulecake
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.anderson.schedulecake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
     }
 }
