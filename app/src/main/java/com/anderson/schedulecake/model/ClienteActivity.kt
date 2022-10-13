3dd7a80b2783bd9d4ceea1a407a42136f901d935
andersons1lva <andersons1lva@yahoo.com.br>
2022-10-13 00:33:48 -0300

    Activity client created

---
+++ app/src/main/java/com/anderson/schedulecake/model/ClienteActivity.kt
@@ -0,0 +1,11 @@
+package com.anderson.schedulecake.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cliente)
+    }
+}
